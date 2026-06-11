2665087cb64c610dfe220852a7752003e0ead76a
Tanmay Gupta <332tanmaygupta@gmail.com>
2026-06-11 21:25:48 +0530

    Day 15 completed

---
+++ DAY 15/reversearray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Reversed array:\n");
+    for(i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
