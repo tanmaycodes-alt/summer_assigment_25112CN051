86f20e16a4f01436e910f2a36b3cc8947df65258
Tanmay Gupta <332tanmaygupta@gmail.com>
2026-06-15 20:52:19 +0530

    Day 19 completed

---
+++ DAY 19/transposematrix.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], t[10][10];
+    int r, c, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter matrix:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            t[j][i] = a[i][j];
+        }
+    }
+
+    printf("Transpose Matrix:\n");
+
+    for(i = 0; i < c; i++)
+    {
+        for(j = 0; j < r; j++)
+        {
+            printf("%d ", t[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
