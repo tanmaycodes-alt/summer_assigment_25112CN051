1f51f31c90bf1a17cfbb3226dc9fe1c9a62ed4a5
Tanmay Gupta <332tanmaygupta@gmail.com>
2026-06-19 21:37:04 +0530

    Day 23 completed

---
+++ DAY 23/find_first_nonrepeating_character.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, j, count;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        count = 0;
+
+        for(j = 0; str[j] != '\0'; j++)
+        {
+            if(str[i] == str[j])
+            {
+                count++;
+            }
+        }
+
+        if(count == 1)
+        {
+            printf("First non-repeating character = %c", str[i]);
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ DAY 23/max_occuring_character.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, j, count, max = 0;
+    char ch;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        count = 0;
+
+        for(j = 0; str[j] != '\0'; j++)
+        {
+            if(str[i] == str[j])
+            {
+                count++;
+            }
+        }
+
+        if(count > max)
+        {
+            max = count;
+            ch = str[i];
+        }
+    }
+
+    printf("Maximum occurring character = %c\n", ch);
+    printf("Frequency = %d", max);
+
+    return 0;
+}
