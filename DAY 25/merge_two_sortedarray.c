88b1d530e60e2f388b2d9dcf9bc92686cc176ed5
Tanmay Gupta <332tanmaygupta@gmail.com>
2026-06-21 20:34:03 +0530

    Day 25 completed

---
+++ DAY 25/merge_two_sortedarray.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], b[100], c[200];
+    int n1, n2, i = 0, j = 0, k = 0;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter elements of first sorted array:\n");
+    for(i = 0; i < n1; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter elements of second sorted array:\n");
+    for(i = 0; i < n2; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+
+    i = 0;
+    j = 0;
+
+    while(i < n1 && j < n2)
+    {
+        if(a[i] < b[j])
+        {
+            c[k] = a[i];
+            i++;
+        }
+        else
+        {
+            c[k] = b[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < n1)
+    {
+        c[k] = a[i];
+        i++;
+        k++;
+    }
+
+    while(j < n2)
+    {
+        c[k] = b[j];
+        j++;
+        k++;
+    }
+
+    printf("Merged sorted array:\n");
+
+    for(i = 0; i < k; i++)
+    {
+        printf("%d ", c[i]);
+    }
+
+    return 0;
+}
